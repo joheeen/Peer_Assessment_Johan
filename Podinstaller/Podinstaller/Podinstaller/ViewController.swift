59dba263c87ed34689a57246da994db6fd0420a7
joheeen <johanvandermeulen_1@hotmail.com>
2015-04-03 12:11:17 +0200

    Pod installer

---
+++ Podinstaller/Podinstaller/Podinstaller/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Podinstaller
+//
+//  Created by Johan van der Meulen on 16/03/15.
+//  Copyright (c) 2015 Johan van der Meulen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var testExSwift = "testing"
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podinstaller/Podinstaller/Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,26 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Realm/Headers
+#define COCOAPODS_POD_AVAILABLE_Realm_Headers
+#define COCOAPODS_VERSION_MAJOR_Realm_Headers 0
+#define COCOAPODS_VERSION_MINOR_Realm_Headers 91
+#define COCOAPODS_VERSION_PATCH_Realm_Headers 1
+
+// exswift
+#define COCOAPODS_POD_AVAILABLE_exswift
+#define COCOAPODS_VERSION_MAJOR_exswift 0
+#define COCOAPODS_VERSION_MINOR_exswift 1
+#define COCOAPODS_VERSION_PATCH_exswift 9
+
+// realm
+#define COCOAPODS_POD_AVAILABLE_realm
+#define COCOAPODS_VERSION_MAJOR_realm 0
+#define COCOAPODS_VERSION_MINOR_realm 91
+#define COCOAPODS_VERSION_PATCH_realm 1
+
